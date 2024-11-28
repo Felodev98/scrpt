e2899e8eac48ccc3deb5a744d2dee5c987f5c6bf
Felodev98 <felixkagecha@gmail.com>
2024-11-28 04:50:46 +0000

    added strings in my index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function greet(name , lastName){
     console.log('Hello ' + name  + ' ' + lastName);
 }
 greet('Ken', 'Mosh');
+
+const message = 'This is my first message'
+console.log(typeof 'This is my first message');
+console.log(message.length);
+console.log(message[0]);
+console.log(message[4]);
+console.log(message.includes('my'));
+console.log(message.includes('not'));
