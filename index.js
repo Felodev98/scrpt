6af96003c18c5e2514aed9b90a00cac1ad4cd02c
Felodev98 <felixkagecha@gmail.com>
2024-11-30 04:59:23 +0000

    successful written a function and called the function

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ console.log(message[0]);
 console.log(message[4]);
 console.log(message.includes('my'));
 console.log(message.includes('not'));
+
+function happyBirthday(username, age){
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday to you!");
+    console.log(`Happy Birthday dear ${username}!`);
+    console.log("Happy Birthday to you!");
+    console.log(`You are ${age} years old`);
+    
+}
+
+happyBirthday("Felix", 27);
